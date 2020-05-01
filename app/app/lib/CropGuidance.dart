4b86fcd7fedbedf953f09c16191ea5adb48d8420
Akhil Sodari <akhil.sodari@gmail.com>
2020-05-01 13:15:04 +0530

    Change-1

---
+++ app/app/lib/CropGuidance.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Encrypt extends StatefulWidget {
+  @override
+  _EncryptState createState() => _EncryptState();
+}
+
+class _EncryptState extends State<Encrypt> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Encrypt'),
+        ),
+        body: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0)),
+          child: Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.all(10.0),)
+
+
+            ],
+
+          ),
+          ),
+        );
+  }
+}
---
+++ app/lib/HomePage.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'Encrypt.dart';
+
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+ 
+ //AssetImage one= AssetImage(images/abc.png);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Farmer Friend'),
+      ),
+      body: Container(
+        child: Column(
+          
+         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            
+            
+            MaterialButton(
+              height: 100.0,
+              onPressed: (){
+                     Navigator.push(
+                         context,
+                        MaterialPageRoute(
+                            builder: (context) => Encrypt()
+                            ),
+                    );
+                  },
+              child: Text('Encrypt The File'),
+              color: Colors.lightGreen,
+              splashColor: Colors.deepPurple,
+            )
+           /* MaterialButton(
+              height: 100.0,
+
+              
+              onPressed: (){
+                     Navigator.push(
+                         context,
+                        MaterialPageRoute(
+                            builder: (context) => Stegun()
+                            ),
+                    );
+                  },
+              child: Text('Steganography'),
+              color: Colors.redAccent,
+            ),
+            MaterialButton(
+              height: 100.0,
+
+              onPressed: (){
+                     Navigator.push(
+                         context,
+                        MaterialPageRoute(
+                            builder: (context) => Decrypt()
+                            ),
+                    );
+                  },
+
+              
+              child: Text('Decrypt The File'),
+              color: Colors.yellow,
+            ),
+            MaterialButton(
+              height: 100.0,
+
+              onPressed: (){},
+              child: Text('Exit'),
+              color: Colors.indigo,
+            ), */
+            
+          ],
+        ),
+      ),
+      
+
+    );
+      
+    
+  }
+}
